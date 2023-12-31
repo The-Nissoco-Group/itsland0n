aae0ea535928e318fcdfe043b1c384bbee3f12c8
hackermon <therealhackermon@gmail.com>
2023-12-30 21:11:00 -0500

    check regex

--- src/moderation/customFilter.ts
+++ src/moderation/customFilter.ts
@@ -15,7 +15,19 @@ function customWordFilter(message: Message, guildConfig: Guild) {
 }
 
 function customRegexFilter(message: Message, guildConfig: Guild) {
-
+    for (const words of message.content.split(" ")) {
+        for (const rawFilter of guildConfig.moderation.customRegexFilter) {
+            const filter = new RegExp(rawFilter);
+            if (filter.test(words)) {
+                try { message.delete() }
+                // Incase bot does not have permission to delete
+                catch { return }
+    
+                try { message.channel.send(`<@${message.author.id}>, the message you sent contained content not allowed on this server!`) }
+                catch { return }
+            }   
+        }
+    }
 }
 
 export { customWordFilter, customRegexFilter }

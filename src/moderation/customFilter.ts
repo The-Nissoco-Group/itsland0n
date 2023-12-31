f6710474b29b3705d89f18d5e42fb6dde10ab3aa
hackermon <therealhackermon@gmail.com>
2023-12-30 21:19:10 -0500

    test the entire message content at once

--- src/moderation/customFilter.ts
+++ src/moderation/customFilter.ts
@@ -15,18 +15,17 @@ function customWordFilter(message: Message, guildConfig: Guild) {
 }
 
 function customRegexFilter(message: Message, guildConfig: Guild) {
-    for (const words of message.content.split(" ")) {
-        for (const rawFilter of guildConfig.moderation.customRegexFilter) {
-            const filter = new RegExp(rawFilter);
-            if (filter.test(words)) {
-                try { message.delete() }
-                // Incase bot does not have permission to delete
-                catch { return }
+    const content = message.content;
+    for (const rawFilter of guildConfig.moderation.customRegexFilter) {
+        const filter = new RegExp(rawFilter);
+        if (filter.test(content)) {
+            try { message.delete() }
+            // Incase bot does not have permission to delete
+            catch { return }
     
-                try { message.channel.send(`<@${message.author.id}>, the message you sent contained content not allowed on this server!`) }
-                catch { return }
-            }   
-        }
+            try { message.channel.send(`<@${message.author.id}>, the message you sent contained content not allowed on this server!`) }
+            catch { return }
+        }   
     }
 }
 

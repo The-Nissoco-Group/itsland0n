62c40d8570a2f3bac237d9b7273a6729f124f043
Nissoco23 <nissocomc@outlook.com>
2023-12-31 14:36:01 -0600

    Fixed missing semicolons

--- src/moderation/detectWebContent.ts
+++ src/moderation/detectWebContent.ts
@@ -4,13 +4,15 @@ import { Guild } from "../configs/guild";
 function detectDiscordInvites(message: Message, guildConfig: Guild) {
     if (!guildConfig.moderation.removeDiscordInvites) return
 
-    const filter = "(https?:\/\/|http?:\/\/)?(www.)?(discord.(gg|io|me|li)|discordapp.com\/invite|discord.com\/invite)\/[^\s\/]+?(?=\b)"
-    const regex = new RegExp(filter)
+    const filter = "(https?:\/\/|http?:\/\/)?(www.)?(discord.(gg|io|me|li)|discordapp.com\/invite|discord.com\/invite)\/[^\s\/]+?(?=\b)";
+    const regex = new RegExp(filter);
 
     if (regex.test(message.content)) {
-        message.delete()
+        message.delete();
 
-        try { message.channel.send(`<a:deny:1190937442203734066> <@${message.author.id}>, discord invites are not allowed on this server!`) }
-        catch { return }
+        try { message.channel.send(`<a:deny:1190937442203734066> <@${message.author.id}>, discord invites are not allowed on this server!`); }
+        catch { return; }
     }
 }
+
+export { detectDiscordInvites }

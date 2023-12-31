85a21c39df825896a100f4def665de71074b5657
Nissoco23 <nissocomc@outlook.com>
2023-12-31 14:52:34 -0600

    Fixed an issue in the regular expression

--- src/moderation/detectWebContent.ts
+++ src/moderation/detectWebContent.ts
@@ -1,16 +1,16 @@
-import { Message } from "discord.js";
+import { GuildDefaultMessageNotifications, Message } from "discord.js";
 import { Guild } from "../configs/guild";
 
 function detectDiscordInvites(message: Message, guildConfig: Guild) {
     if (!guildConfig.moderation.removeDiscordInvites) return
 
-    const filter = "(https?:\/\/|http?:\/\/)?(www.)?(discord.(gg|io|me|li)|discordapp.com\/invite|discord.com\/invite)\/[^\s\/]+?(?=\b)";
+    const filter = "(https?:\\/\\/|http?:\\/\\/)?(www.)?(discord.(gg|io|me|li)|discordapp.com\\/invite|discord.com\\/invite)\\/[^\\s\\/]+?(?=\\b)";
     const regex = new RegExp(filter);
 
     if (regex.test(message.content)) {
         message.delete();
 
-        try { message.channel.send(`<a:deny:1190937442203734066> <@${message.author.id}>, discord invites are not allowed on this server!`); }
+        try { message.channel.send(`<a:deny:1190937442203734066> <@${message.author.id}>, Discord invites are not allowed on this server!`); }
         catch { return; }
     }
 }
